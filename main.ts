b8bd4308593fa974b189fe4982896fa323a7d36b
Ash <ashwsng@proton.me>
2024-09-02 18:13:04 +0200

    uhm

--- main.ts
+++ main.ts
@@ -7,20 +7,13 @@ interface RGBinterface {
 }
 
 class Individual {
-  key: string;
   color?: RGBinterface;
   parent1?: Individual;
   parent2?: Individual;
 
-  constructor(key: string, color: RGBinterface);
-  constructor(key: string, parent1: Individual, parent2: Individual);
-  constructor(
-    arg1: string,
-    arg2: RGBinterface | Individual,
-    arg3?: Individual
-  ) {
-    this.key = arg1;
-
+  constructor(color: RGBinterface);
+  constructor(parent1: Individual, parent2: Individual);
+  constructor(arg2: RGBinterface | Individual, arg3?: Individual) {
     if (isRGBinterface(arg2)) {
       this.color = arg2;
       this.parent1 = undefined;
@@ -31,11 +24,26 @@ class Individual {
       this.color = undefined;
     }
   }
-
-  // TODO: procreation. Store in array of individuals.
 }
 
 //Functions
+
+function procreate(IndividualKey:string) : void{
+
+
+}
+
+function getParents(IndividualKey:string) : Individual | undefined {
+    const child : Individual = IndividualsDictionary[IndividualKey] //gets the individual we're referring to by using the provided key
+
+    if (!child) {
+        console.log("Cannot find child with provided key in dictionary. Contact your Ash for more info.");
+        return undefined;
+    }
+    else {
+        return 
+    }
+}
 
 function isRGBinterface(arg: any): arg is RGBinterface {
   //if all of these conditions are true, it tells ts that within the scope the function is called,
@@ -51,14 +59,11 @@ function isRGBinterface(arg: any): arg is RGBinterface {
 
 //Data
 
-let IndividualArray: Individual[] = [
-  new Individual("ancestorRed", { R: 255, G: 0, B: 0 }),
-  new Individual("ancestorGreen", { R: 0, G: 255, B: 0 }),
-  new Individual("ancestorBlue", { R: 0, G: 0, B: 255 }),
-];
-
-//HOW TO USE:
-// run program.
-// 1. create new Individual from desired parents
-// 2. call procreate() on Individual's key to assign them their color.
-// 3. use console.log on the Individual's color property to find out their color.
+const IndividualsDictionary: { [key: string]: Individual } = {
+  ancestorRed: new Individual({ R: 255, G: 0, B: 0 }),
+  ancestorGreen: new Individual({ R: 0, G: 255, B: 0 }),
+  ancestorBlue: new Individual({ R: 0, G: 0, B: 255 }),
+  ancestorWhite: new Individual({ R: 255, G: 255, B: 255 }),
+  ancestorBlack: new Individual({ R: 0, G: 0, B: 0 }),
+  hardcodedIndividual: new Individual("")
+};
